0d4a001aba8a20fa818a4d97c692ef00153b7c51
rinodipaola15 <46008803+rinodipaola15@users.noreply.github.com>
2020-10-26 19:45:17 +0100

    Update guestbook.js

--- docs/guestbook.js
+++ docs/guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://9647a6ad.eu-gb.apigw.appdomain.cloud/guestbook';
+const apiUrl = 'https://fab8a730.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
